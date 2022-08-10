fa2d8a06277de78fad247631e42c8302377e5175
Anais Roger <anais.roger@mail.mcgill.ca>
2022-08-10 13:52:21 -0400

    added seed and validations all pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ Restaurant.destroy_all
 
 puts "Seeding restaurants..."
 
-100.times do
+10.times do
   rest = Restaurant.create!(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
-    phone_number: Faker::PhoneNumber.phone_number
-    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
   )
   puts "Restaurant with id #{rest.id} was added"
 end
